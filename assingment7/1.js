72df6efce47ee731505a655a7d3172ec6754ba59
ismailgithubrepo <165195430+ismailgithubrepo@users.noreply.github.com>
2024-05-24 21:46:21 -0700

    Add files via upload

---
+++ assingment7/1.js
@@ -0,0 +1,8 @@
+let mulOf3=[]
+
+for(i=1;i<=100;i++){
+    if(i%3==0)
+        mulOf3.push(i)
+}
+console.log('multiple of 3 between 0 to 100 are:')
+console.log(mulOf3)
---
+++ assingment7/2.js
@@ -0,0 +1,7 @@
+function countnum(a){
+    for(i=0;i<=a;i++){
+        console.log(i)
+    }
+}
+console.log('counting from 0 to 100')
+countnum(100)
